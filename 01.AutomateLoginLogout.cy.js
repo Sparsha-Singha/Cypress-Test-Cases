d141ce498299c4d51eb4f53e55f34cb344e7b52b
Sparsha-Singha <sparshasourav19@gmail.com>
2024-01-11 04:16:47 +0600

    login commited

---
+++ 01.AutomateLoginLogout.cy.js
@@ -0,0 +1,13 @@
+/// <reference types = "cypress"/>
+
+describe('Validate login functionality',()=>{
+    
+
+    it('with valid credential',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.oxd-button').click();
+    })
+
+})
