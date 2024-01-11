85356f495de455304c28145ae62aa2e893a7c559
Sparsha-Singha <sparshasourav19@gmail.com>
2024-01-11 07:30:45 +0600

    4th commit

---
+++ 05.Validate dropdowns.cy.js
@@ -0,0 +1,19 @@
+/// <reference types = "cypress"/>
+
+describe('validate select based dropdown', ()=>{
+
+    before('visit & Login', ()=>{
+        cy.visit('https://www.saucedemo.com/v1/index.html')
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+
+    })
+    it('Select from dropdown', ()=>{
+        cy.get('.product_sort_container').select('Price (high to low)')
+        cy.get('.product_sort_container').select('Price (low to high)')
+        cy.get('.product_sort_container').select('Name (A to Z)')
+        cy.get('.product_sort_container').select('Name (Z to A)')
+        
+    })
+})
